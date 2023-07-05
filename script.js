c7cf0b9203b991b42c58f927956889ed98c4c0b5
Gaurav Soni <120136122+gaurav8158@users.noreply.github.com>
2023-07-05 23:10:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function firstChar(text) {
   // your code here
 	if(text.length===0) return "";
+	text.trim();
+	if(text.length===0) return "";
 	return text.slice(0,1);
 }
 
 // Do not change the code below
 
- const text = prompt("Enter text:");
-alert(firstChar(text));
+ // const text = prompt("Enter text:");
+// alert(firstChar(text));
